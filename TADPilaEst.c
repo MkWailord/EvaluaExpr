fbc1df5aae20ee3cc246f40fdce903e3974714de
Jose Casas <31327191+MkWailord@users.noreply.github.com>
2017-08-24 19:24:20 -0500

    Implementación de librería de la pila estática.
    Contiene las funciones que serán usadas por el TAD PilaEst.
    Vista disponible para: Implementador.
    Puede ser pre compilada a TADPilaEst.o (En consola: "gcc TADPilaEst.c -c")
    Creada en el salón el día 14/08/2017.

---
+++ TADPilaEst.c
@@ -0,0 +1,51 @@
+#include "TADPilaEst.h"
+
+void Initialize(pila *s)
+{
+	(*s).tope=-1; //s->tope=-1; 
+	return;
+}
+
+void Push(pila *s, elemento e)
+{
+  (*s).tope++;
+  (*s).arreglo[(*s).tope]=e;
+  return;
+}
+
+elemento Pop(pila *s)
+{
+  elemento e;
+  e=(*s).arreglo[(*s).tope];
+  (*s).tope--;
+  return e;
+}
+
+boolean Empty(pila *s)
+{
+  if((*s).tope==-1)
+    b=TRUE;
+  else
+    b=FALSE;
+  return b;
+}
+
+elemento Top(pila *s)
+{
+  elemento e;
+  e=(*s).arreglo[(*s).tope];
+  return e;
+}
+
+int Size(pila *s)
+{
+  int size;
+  size=(*s).tope+1;
+  return size;
+}
+
+void Destroy(pila *s)
+{
+	Initialize(s);
+	return;
+}
