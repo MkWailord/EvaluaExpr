95ba66c87bd01819e390f558341bcb21e27d9ec7
Jose Casas <31327191+MkWailord@users.noreply.github.com>
2017-08-30 12:45:46 -0500

    Update Parentesis.c

--- Parentesis.c
+++ Parentesis.c
@@ -12,11 +12,25 @@ main()
 	Initialize(pila1);	
 	tamCadena=strlen(expresion);
 	
-	scanf("%s", expresion);
+	scanf("%s", expresion)
 	
-	
-	
-	
-	
+	for(i=0;i<=tamCadena;i++)
+	{
+		if(expresion[i]=='(')
+		{
+			e1.c='(';
+			push(&pila1, e1);
+		}
+		else if(expresion[i]==')')
+		{
 
+			if(Empty(&pila1)==1)
+				{
+					printf("ERROR");
+					exit(1);
+				}
+			e1=pop(&pila1)
+		}
+	}
+	
 }
